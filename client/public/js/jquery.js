b75097776222a2728f740d45495919bb6a1f5f6a
Justin Stout (DA) <JustinS@healthaxisgroup.com>
2018-11-12 12:37:16 -0600

    Cleanup comments in web_server and jquery.js. Add acorn to package-json

--- client/public/js/jquery.js
+++ client/public/js/jquery.js
@@ -1,28 +1,26 @@
-$(function(){
-    $(".button").click(function(){
-        let userInput = {
-            subreddit: $("#name").val(),
-            number: $("#number").val()
-        };
+$(function() {
+	$('.button').click(function(){
+		let userInput = {
+			subreddit: $('#name').val(),
+			number: $('#number').val()
+		};
 
-        console.log(userInput);
+		console.log(userInput);
 
-        $.ajax({
-            type: 'POST', // added
-            url: '/api/subreddit-info',
-            data: userInput,
-            success: function(post_listings) {
-                //let ret = jQuery.JSON.parse(data);
-                // alert(($("input.textbox").val()));
-                // alert();
-                console.log("Success!");
-                console.log(userInput);
-                console.log(post_listings);
-            },
-            error: function (xhr, status, error) {
-                alert('Error: ' + error.message);
-            }
-        });
-    });
+		$.ajax({
+			type: 'POST', // added
+			url: '/api/subreddit-info',
+			data: userInput,
+			success: function(post_listings) {
+
+				console.log('Success!');
+				console.log(userInput);
+				console.log(post_listings);
+			},
+			error: function (xhr, status, error) {
+				alert('Error: ' + error.message);
+			}
+		});
+	});
 });
 
